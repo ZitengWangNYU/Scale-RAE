68f118c987306ddb800df412bb08f709f4c070a6
Ziteng Wang <zw2526@nyu.edu>
2026-02-11 21:06:47 -0800

    added .whl file

--- install_spmd.sh
+++ install_spmd.sh
@@ -2,12 +2,8 @@ pip install --upgrade pip
 pip install -e '.[tpu]'
 pip install torch==2.6.0 && pip install torchvision==0.21.0
 
-# Download TorchXLA wheel
-# You can use your own hosting or the official releases
-# Example: gsutil cp gs://your-bucket/torch_xla-2.6.0+git0bb4f6f-cp310-cp310-linux_x86_64.whl .
-wget https://storage.googleapis.com/pytorch-xla-releases/wheels/tpuvm/torch_xla-2.6.0-cp310-cp310-linux_x86_64.whl
-
-pip install torch_xla-2.6.0-cp310-cp310-linux_x86_64.whl
+# torch_xla-2.6.0+git0bb4f6f-cp310-cp310-linux_x86_64.whl is provided in the repo
+pip install torch_xla-2.6.0+git0bb4f6f-cp310-cp310-linux_x86_64.whl
 pip install 'torch_xla[tpu]' -f https://storage.googleapis.com/libtpu-releases/index.html
 pip install 'torch_xla[pallas]' -f https://storage.googleapis.com/jax-releases/jax_nightly_releases.html -f https://storage.googleapis.com/jax-releases/jaxlib_nightly_releases.html
 pip install zstandard
